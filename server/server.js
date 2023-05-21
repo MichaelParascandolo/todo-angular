646af44b13e795c768d7e503f267d0c4b5072bbc
Michael Parascandolo <michaelparascandolo@me.com>
2023-05-20 21:30:20 -0400

    connecting express to mysql

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require("mysql");
 
+// database connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "todo",
+});
+// connect to database
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+// test endpoint
 app.get("/api/data", (req, res) => {
-  const data = { message: "Hello from the server!" };
+  const data = { message: "Hello from the server!!" };
   res.send(data);
 });
 
